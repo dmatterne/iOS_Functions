5def6b1c2f9fa873421184f63bf41ff321ed095d
David Matterne <david.matterne@gmail.com>
2016-09-27 11:06:23 +0200

    Initial Commit

---
+++ iOS_Functions/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  iOS_Functions
+//
+//  Created by Stannis Baratheon on 27/09/16.
+//  Copyright © 2016 Training. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
